9aaa1ef6df30433a443fd93245aae3fb87e7523a
Petrik Diak <diak@petrik.hu>
2026-01-14 11:28:55 +0100

    implement form submission behaviour

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,11 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+document.querySelector<HTMLButtonElement>('button')!.addEventListener('click', e => {
+  e.preventDefault();
+  
+  document.querySelectorAll<HTMLInputElement>('input').forEach(it => it.value = '')
+  document.querySelector<HTMLDivElement>('#alert-success1')!.classList.remove('hidden');
+})
